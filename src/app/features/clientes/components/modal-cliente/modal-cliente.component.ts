2e5d7eb607a459a559abde678de0351294463d50
Michael Faleiro <michaelfaleiro@gmail.com>
2024-10-13 21:08:49 -0300

    Features Orçamento, Orcamento-Cotacao

---
+++ src/app/features/clientes/components/modal-cliente/modal-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-cliente.component.html',
+  styleUrl: './modal-cliente.component.css'
+})
+export class ModalClienteComponent {
+
+}
